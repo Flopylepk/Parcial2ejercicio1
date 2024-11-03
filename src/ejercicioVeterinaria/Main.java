f87bdcf1e3c53a6ab2c791ea9ef962010f72dfdc
Flopylepk <florencia.torres@davinci.edu.ar>
2024-11-03 11:39:54 -0300

    cliente

--- src/ejercicioVeterinaria/Main.java
+++ src/ejercicioVeterinaria/Main.java
@@ -15,7 +15,7 @@ public class Main {
 		
 		String[] menu= {"Veterinario","Cliente", "Salir"};
 		String[] menuV= {"Registrarse","ver cliente","ver registro de animales","revisar animales","curar animales","ver mi registro" ,"Salir"};
-		String[] menuC= {"Registrarse","ver veterinario","ver estado de animales","ver registro de animales","ver mi registro" , "Salir"};
+		String[] menuC= {"Registrarse","registrar animales","ver veterinario","ver estado de animales","ver registro de animales","ver mi registro" , "Salir"};
 		
 		int opcion=0;
 		int opcionV=0;
@@ -86,7 +86,64 @@ public class Main {
 				break;
 			case 1:
 				//cliente
-				
+				do {
+					opcionC=JOptionPane.showOptionDialog(null, "Elija que hacemos", null, 0, 0, null, menuC, menuC[0]);
+					switch (opcionC) {
+					case 0:
+						//Registrarse
+						if (cliente.getNombre().isEmpty()) {
+							cliente.Registrarse();
+						}else {
+							JOptionPane.showMessageDialog(null, "usted ya se registro");
+						}
+						break;
+					case 1:
+						//ver veterinario
+						if (veterinario.getNombre().isEmpty()) {
+							JOptionPane.showMessageDialog(null, "EL veterinario no se registro");
+						}else {
+							JOptionPane.showMessageDialog(null, veterinario);
+						}
+						break;
+					case 2:
+						//registrar animales
+						if (cliente.getNombre().isEmpty()) {
+							JOptionPane.showMessageDialog(null, "deve registrarse primero");
+						}else {
+							cliente.RegistrarAnimales(perro, gato, ave);
+						}
+						break;
+					case 3:
+						//ver estado de animales
+						if (cliente.getNombre().isEmpty()) {
+							JOptionPane.showMessageDialog(null, "deve registrarse primero");
+						}else {
+							cliente.RevisarEstado(perro, gato, ave);
+						}
+						break;
+					case 4:
+						//ver registro de animales
+						if (cliente.getNombre().isEmpty()) {
+							JOptionPane.showMessageDialog(null, "deve registrarse primero");
+						}else {
+							cliente.VerAnimales(perro, gato, ave);
+						}
+						break;
+					case 5:
+						//ver mi registro
+						if (cliente.getNombre().isEmpty()) {
+							JOptionPane.showMessageDialog(null, "deve registrarse primero");
+						}else {
+							JOptionPane.showMessageDialog(null, cliente);
+						}
+						break;
+					case 6:
+						//Salir
+						JOptionPane.showMessageDialog(null, "nos vemos la proxima");
+						break;
+					
+					}
+				} while (opcionC!=6);
 				break;
 			case 2:
 				//salir
